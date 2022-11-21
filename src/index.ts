b834aa2fd7229d96f7ddc9122b0bdcb045e5baa4
rivaldoSouzaS <rivaldosilva@si.fiponline.edu.br>
2022-11-21 17:58:29 -0300

    teste git

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 const port = 3001;
-
+//teste git
 app.listen(3001, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
