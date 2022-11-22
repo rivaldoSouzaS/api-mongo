ee2b11657b6fdd62f632231adc18c5718851ee7a
rivaldoSouzaS <rivaldosilva@si.fiponline.edu.br>
2022-11-22 15:04:16 -0300

    conect with mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-
-const port = 3001;
-//teste git
-app.listen(3001, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+mongoose.connect('mongodb://localhost:27017').then(() => {
+	console.log('Conectado ao mongodb');
+	const app = express();
+	const port = 3001;
+	app.listen(3001, () => {
+		console.log(`Server is running on http://localhost:${port}`);
+	});
+}).catch(()=>{
+	console.log('Error');
 });
+
+
